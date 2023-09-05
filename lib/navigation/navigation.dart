0705b6c4acf7d18f5168042c6815767edba43967
samro123 <thsam.20it5@vku.udn.vn>
2023-09-05 09:31:41 +0700

    #4 sam create onboarding layout

--- lib/navigation/navigation.dart
+++ lib/navigation/navigation.dart
@@ -5,6 +5,7 @@ import '../blocs/authentication/authentication_cubit.dart';
 import '../common/error_screen.dart';
 import '../screens/home/home.dart';
 import '../screens/login/login.dart';
+import '../screens/onboarding/onboarding_screen.dart';
 
 abstract class NavigationPath {
   NavigationPath._();
@@ -18,7 +19,7 @@ abstract class AppRouter {
 
   static final routerConfig = GoRouter(
     debugLogDiagnostics: true,
-    initialLocation: NavigationPath.login,
+    initialLocation: NavigationPath.onboarding,
     redirect: (context, _) {
       if (context.read<AuthenticationCubit>().state) {
         return NavigationPath.home;
@@ -27,10 +28,6 @@ abstract class AppRouter {
       }
     },
     routes: [
-      // GoRoute(
-      //   path: NavigationPath.onboarding,
-      //   builder: (_, __) => const OnboardingScreen(),
-      // ),
       GoRoute(
         path: NavigationPath.home,
         builder: (_, __) => const HomeScreen(),
@@ -38,6 +35,10 @@ abstract class AppRouter {
       GoRoute(
         path: NavigationPath.login,
         builder: (_, __) => const LoginScreen(),
+      ),
+      GoRoute(
+        path: NavigationPath.onboarding,
+        builder: (_, __) => const Onboarding(),
       ),
     ],
     errorBuilder: (_, __) => const ErrorScreen(),
---
+++ lib/screens/onboarding/component/body.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+import 'card_carouse.dart';
+
+class Body extends StatelessWidget {
+  const Body({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return CardCarouse();
+  }
+}
+
+
