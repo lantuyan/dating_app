c0711ca50c46fe39c814dcaa07db22ed04583bf6
lantuyan <nqlan.20it4@vku.udn.vn>
2023-09-26 21:40:30 +0700

    create match, chat, message model

--- lib/models/model.dart
+++ lib/models/model.dart
@@ -1,1 +1,4 @@
 export 'user/user_model.dart';
+export 'user/user_match_model.dart';
+export 'user/chat_model.dart';
+export 'user/message_model.dart';
---
+++ lib/models/user/message_model.dart
@@ -0,0 +1,114 @@
+import 'package:equatable/equatable.dart';
+import 'package:intl/intl.dart';
+
+class Message extends Equatable {
+  final int id;
+  final int senderId;
+  final int receiverId;
+  final String message;
+  final DateTime dateTime;
+  final String timeString;
+
+  const Message({
+    required this.id,
+    required this.senderId,
+    required this.receiverId,
+    required this.message,
+    required this.dateTime,
+    required this.timeString
+  });
+
+  @override
+  // TODO: implement props
+  List<Object?> get props => [
+        id,
+        senderId,
+        receiverId,
+        message,
+        dateTime,
+        timeString,
+      ];
+
+  static List<Message> matches = [
+    Message(
+      id: 1,
+      senderId: 1,
+      receiverId: 2,
+      message: "Hello",
+      dateTime: DateTime.now(),
+      timeString: DateFormat('jm').format(DateTime.now()),
+    ),
+    Message(
+      id: 2,
+      senderId: 2,
+      receiverId: 1,
+      message: "Hi",
+      dateTime: DateTime.now(),
+      timeString: DateFormat('jm').format(DateTime.now()),
+    ),
+    Message(
+      id: 3,
+      senderId: 1,
+      receiverId: 2,
+      message: "How are you?",
+      dateTime: DateTime.now(),
+      timeString: DateFormat('jm').format(DateTime.now()),
+    ),
+    Message(
+      id: 4,
+      senderId: 2,
+      receiverId: 1,
+      message: "I'm fine",
+      dateTime: DateTime.now(),
+      timeString: DateFormat('jm').format(DateTime.now()),
+    ),
+    Message(
+      id: 5,
+      senderId: 1,
+      receiverId: 2,
+      message: "What are you doing?",
+      dateTime: DateTime.now(),
+      timeString: DateFormat('jm').format(DateTime.now()),
+    ),
+    Message(
+      id: 6,
+      senderId: 2,
+      receiverId: 1,
+      message: "I'm working",
+      dateTime: DateTime.now(),
+      timeString: DateFormat('jm').format(DateTime.now()),
+    ),
+    Message(
+      id: 7,
+      senderId: 1,
+      receiverId: 2,
+      message: "What about you?",
+      dateTime: DateTime.now(),
+      timeString: DateFormat('jm').format(DateTime.now()),
+    ),
+    Message(
+      id: 8,
+      senderId: 2,
+      receiverId: 1,
+      message: "I'm working too",
+      dateTime: DateTime.now(),
+      timeString: DateFormat('jm').format(DateTime.now()),
+    ),
+    Message(
+      id: 9,
+      senderId: 1,
+      receiverId: 2,
+      message: "What are you working on?",
+      dateTime: DateTime.now(),
+      timeString: DateFormat('jm').format(DateTime.now()),
+    ),
+    Message(
+      id: 10,
+      senderId: 2,
+      receiverId: 1,
+      message: "I'm working on a project",
+      dateTime: DateTime.now(),
+      timeString: DateFormat('jm').format(DateTime.now()),
+    ),
+  ];
+}
