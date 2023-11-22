e64c2de2fb8ddcca4e40160859aefe405493a80b
lantuyan <nqlan.20it4@vku.udn.vn>
2023-11-22 12:24:49 +0700

    Update add Firebase initialization and import dependencies

--- lib/main_production.dart
+++ lib/main_production.dart
@@ -1,14 +1,20 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 import 'app_flavor.dart';
 import 'bootstrap.dart';
 import 'di/service_locator.dart';
+import 'firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   ServiceLocatorInitializer.initWithFlavor(AppFlavor.staging);
-  
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  // BindingBase.debugZoneErrorsAreFatal = true;
 
   bootstrap();
 }
