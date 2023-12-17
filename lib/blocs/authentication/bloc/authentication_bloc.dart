c65294aefcd1dc4ff03d86ef9569dae2b416dc2f
lantuyan <nqlan.20it4@vku.udn.vn>
2023-12-18 00:20:16 +0700

    Add template login, register, verify with email using Bloc pattern

---
+++ lib/blocs/authentication/bloc/authentication_bloc.dart
@@ -0,0 +1,33 @@
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+import 'authentication_repository.dart';
+import 'user_model.dart';
+
+part 'authentication_event.dart';
+part 'authentication_state.dart';
+
+class AuthenticationBloc
+    extends Bloc<AuthenticationEvent, AuthenticationState> {
+  final AuthenticationRepository _authenticationReposistory;
+
+  AuthenticationBloc(this._authenticationReposistory)
+      : super(AuthenticationInitial()) {
+    on<AuthenticationEvent>((event, emit) async {
+      if (event is AuthenticationStarted) {
+        UserModel user = await _authenticationReposistory.getCurrentUser().first;
+        if (user.uid != "uid") {
+          String? displayName = await _authenticationReposistory.retrieveUserName(user);
+          emit(AuthenticationSuccess(displayName: displayName));
+        } else {
+          emit(AuthenticationFailure());
+        }
+      }
+      else if(event is AuthenticationSignedOut){
+        await _authenticationReposistory.signOut();
+        emit(AuthenticationFailure());
+      }
+    });
+  }
+}
---
+++ lib/blocs/database/database_repository_impl.dart
@@ -0,0 +1,23 @@
+
+
+import '../authentication/bloc/user_model.dart';
+import 'database_service.dart';
+
+class DatabaseRepositoryImpl implements DatabaseRepository {
+  DatabaseService service = DatabaseService();
+
+  @override
+  Future<void> saveUserData(UserModel user) {
+    return service.addUserData(user) as Future<void>;
+  }
+
+  @override
+  Future<List<UserModel>> retrieveUserData() {
+    return service.retrieveUserData();
+  }
+}
+
+abstract class DatabaseRepository {
+  Future<void> saveUserData(UserModel user);
+  Future<List<UserModel>> retrieveUserData();
+}
